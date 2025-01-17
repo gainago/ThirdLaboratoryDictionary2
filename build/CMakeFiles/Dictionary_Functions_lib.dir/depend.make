45e185e8dfefadda5dcbe7a19a194a8f03c7d93d
gainago <gainagosha@gmail.com>
2025-01-17 13:10:39 +0300

    цуцу

---
+++ build/CMakeFiles/Dictionary_Functions_lib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Dictionary_Functions_lib.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/Dictionary_TypesOfData_lib.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dictionary_TypesOfData_lib.dir/Definition/TypesOfData/Comporators.cpp.o"
+  "CMakeFiles/Dictionary_TypesOfData_lib.dir/Definition/TypesOfData/Comporators.cpp.o.d"
+  "CMakeFiles/Dictionary_TypesOfData_lib.dir/Definition/TypesOfData/MyString.cpp.o"
+  "CMakeFiles/Dictionary_TypesOfData_lib.dir/Definition/TypesOfData/MyString.cpp.o.d"
+  "CMakeFiles/Dictionary_TypesOfData_lib.dir/Definition/TypesOfData/Person.cpp.o"
+  "CMakeFiles/Dictionary_TypesOfData_lib.dir/Definition/TypesOfData/Person.cpp.o.d"
+  "libDictionary_TypesOfData_lib.pdb"
+  "libDictionary_TypesOfData_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dictionary_TypesOfData_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
